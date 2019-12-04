875b2dbccfb5ff7ce75d337cc90fcb67517e1b6a
Sergey <serhiogames@mail.ru>
2019-12-04 23:25:26 +0300

    HomeWorkVer_3

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ let appData = {
 let answer1 = prompt("Введите обязательную статью расходов в этом месяце"),
     answer2 = prompt("Во сколько обойдется"),
     answer3 = prompt("Введите обязательную статью расходов в этом месяце"),
-    answer4 = prompt("Во сколько обойдется"),
+    answer4 = prompt("Во сколько обойдется");
 
 appData["expenses"] = {
     [answer1] : answer2
-};
+}
 appData["expenses"] = {
     [answer3] : answer4
 };
