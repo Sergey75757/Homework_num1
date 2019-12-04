7add4066eeba95bef91e1b0cd1d23362219ea716
Sergey <serhiogames@mail.ru>
2019-12-04 23:19:21 +0300

    HomeWork

--- script.js
+++ script.js
@@ -2,22 +2,25 @@ let money = prompt("Ваш бюджет на месяц");
 let time = prompt("Введите дату в формате YYYY-MM-DD");
 
 let appData = {
-                    money,
-                    time, 
-                    expenses : {}, 
-                    optionalExpenses : {},
-                    income : [],
-                    savings : false
-               };
+    budget: money,
+    dataTime: time, 
+    expenses : {}, 
+    optionalExpenses : {},
+    income : [],
+    savings : false
+};
 
-let answer1 = prompt("Введите обязательную статью расходов в этом месяце");
-let answer2 = prompt("Во сколько обойдется");
+let answer1 = prompt("Введите обязательную статью расходов в этом месяце"),
+    answer2 = prompt("Во сколько обойдется"),
+    answer3 = prompt("Введите обязательную статью расходов в этом месяце"),
+    answer4 = prompt("Во сколько обойдется"),
 
 appData["expenses"] = {
     [answer1] : answer2
 };
+appData["expenses"] = {
+    [answer3] : answer4
+};
 
 var money_day = money / 30;
 alert('Ваш приблизительный бюджет на день: ' + money_day);
-
-console.log(appData);
