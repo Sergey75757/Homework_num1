e25209122560da2557e5c88ab4b5a53664c21864
Sergey <serhiogames@mail.ru>
2020-01-09 15:45:31 +0300

    'addModal'

--- 9/js/script.js
+++ 9/js/script.js
@@ -83,4 +83,31 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     setClock('timer', deadLine);
+
+    //Modal
+
+    let more = document.querySelector('.more'),
+        descriptionBtn = document.querySelectorAll('.description-btn'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function () {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    descriptionBtn.forEach(function(item) {
+        item.addEventListener('click', function callModal() {
+            overlay.style.display = 'block';
+            this.classList.add('more-splash');
+            document.body.style.overflow = 'hidden';
+        })
+    })
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
 })
